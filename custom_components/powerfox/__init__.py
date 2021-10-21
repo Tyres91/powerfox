cbe78d2f370fd6df80ffd5bffb7ecde31831a460
Tyres91 <info@rocky-tyres.de>
2021-10-21 18:16:21 +0200

    Test

--- custom_components/powerfox/__init__.py
+++ custom_components/powerfox/__init__.py
@@ -1,69 +1,8 @@
-"""The powerfox integration."""
-import asyncio
-
-import voluptuous as vol
-from homeassistant.config_entries import ConfigEntry
-from homeassistant.const import (
-    ATTR_ID,
-    ATTR_NAME,
-    CONF_HOST,
-    CONF_PASSWORD,
-    CONF_USERNAME,
-)
-from homeassistant.core import HomeAssistant
-from homeassistant.helpers import config_validation as cv
-
-from .const import DOMAIN
-
-# List of the platforms that we support.
-PLATFORMS = ["switch"]
-
-HOST_CONFIG_SCHEMA = vol.Schema(
-    {
-        vol.Required(CONF_HOST): cv.string,
-        vol.Optional(CONF_USERNAME, default="admin"): cv.string,
-        vol.Required(CONF_PASSWORD): cv.string,
-    }
-)
-
-CONFIG_SCHEMA = vol.All(
-    vol.Schema(
-        {DOMAIN: vol.All(cv.ensure_list, [HOST_CONFIG_SCHEMA])},
-        extra=vol.ALLOW_EXTRA,
-    ),
-)
-##CONFIG_SCHEMA = vol.Schema({DOMAIN: vol.Schema({})}, extra=vol.ALLOW_EXTRA)
+DOMAIN = "hello_state"
 
 
-async def async_setup(hass: HomeAssistant, config: dict):
-    """Set up the powerfox component."""
+def setup(hass, config):
+    hass.states.set("hello_state.world", "Paulus")
+
+    # Return boolean to indicate that initialization was successful.
     return True
-
-
-async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry):
-    """Set up powerfox from a config entry."""
-    # TODO Store an API object for your platforms to access
-    # hass.data[DOMAIN][entry.entry_id] = MyApi(...)
-
-    for component in PLATFORMS:
-        hass.async_create_task(
-            hass.config_entries.async_forward_entry_setup(entry, component)
-        )
-
-    return True
-
-
-async def async_unload_entry(hass: HomeAssistant, entry: ConfigEntry):
-    """Unload a config entry."""
-    unload_ok = all(
-        await asyncio.gather(
-            *[
-                hass.config_entries.async_forward_entry_unload(entry, component)
-                for component in PLATFORMS
-            ]
-        )
-    )
-    if unload_ok:
-        hass.data[DOMAIN].pop(entry.entry_id)
-
-    return unload_ok
